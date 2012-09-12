818ddf1ded1f0dc038f3accdd70650ef9cf37246
Steven Armstrong <steven.armstrong@inf.ethz.ch>
2012-09-12 10:01:54 +0200

    implement BoundCconfig
    Signed-off-by: Steven Armstrong <steven.armstrong@inf.ethz.ch>

--- cconfig.py
+++ cconfig.py
@@ -212,3 +212,39 @@ class Cconfig(collections.MutableMapping):
                     self.__write(path, value)
                 elif os.path.isfile(path):
                     os.unlink(path)
+
+
+class BoundCconfig(Cconfig):
+    """A cconfig object which is bound to a directory.
+    """
+    def __init__(self, path, **kwargs):
+        super(BoundCconfig, self).__init__(**kwargs)
+        self.path = path
+        self._dirty = set()
+        self.from_dir(self.path)
+
+    def __getitem__(self, key):
+        log.debug('__getitem__: {}'.format(key))
+        return super(BoundCconfig, self).__getitem__(key)
+
+    def __setitem__(self, key, value):
+        log.debug('__setitem__: {} = {}'.format(key, value))
+        if not key in self or value != self[key]:
+            super(BoundCconfig, self).__setitem__(key, value)
+            self._dirty.add(key)
+
+    def sync(self):
+        """Sync this cconfig object to disk.
+        """
+        if self._dirty:
+            self.to_dir(self.path)
+
+    def __enter__(self):
+        self.sync()
+        self.from_dir(self.path)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.sync()
+        # we don't handle errors ourself
+        return False
